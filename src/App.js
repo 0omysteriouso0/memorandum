967eafb017d0ffd248c5b321ab9b1fca56cd6520
0omysteriouso0 <paulhsu1016@gmail.com>
2023-04-01 23:25:33 +0800

    good

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import "./styles.css";
+import Fool from "./Fool";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Fool />
+    </div>
+  );
+}
---
+++ src/Fool.js
@@ -0,0 +1,16 @@
+import "./fool.css";
+import React from "react";
+import videoBg from "./video/rick.mp4";
+
+export default function Fool() {
+  return (
+    <div className="main">
+      <div className="overlay"></div>
+      <video src={videoBg} autoPlay loop />
+      <div className="content">
+        <h1>~愚人節快樂~</h1>
+        {/* <p>To my site.</p> */}
+      </div>
+    </div>
+  );
+}
